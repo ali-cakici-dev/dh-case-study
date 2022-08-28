2c19240413d039947f3921b9ddeeee8bce024850
alickc <ali.cakici@eyedius.com>
2022-08-28 17:08:08 +0300

    heroku db error fix try 1

--- dh_flask_app/settings.py
+++ dh_flask_app/settings.py
@@ -1,3 +1,6 @@
 import os
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///db.sqlite3'
